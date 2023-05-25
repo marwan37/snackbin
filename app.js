8b9e04a19c5873508b6bab8fde3ba06e80f25d44
Marwan Zaarab <marwan.zaarab@gmail.com>
2023-05-25 10:46:21 -0500

    modify github table fields

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const config = require("./utils/config");
 
-const binControllers = require("./routes/bin-controllers")(client);
-const binRoutes = require("./routes/bin-routes")(binControllers);
+const binRoutes = require("./routes/bin-routes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const mongoose = require("mongoose");
-const Request = require("./models/request");
-const GithubPayload = require("./models/github-payload");
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -21,47 +18,11 @@ mongoose
 
 app.use("/", binRoutes);
 
-app.post("/payload", async (req, res) => {
-  let collection = await db.collection("test2");
-  await collection.insertOne(req.body);
-  res.send("ok");
-});
+app.use(express.static("build"));
 
-app.get("/requests", async (req, res) => {
-  let collection = await db.collection("test2");
-  let list = await collection.find().toArray();
-  console.log(list);
-  res.send(list);
-});
-
-// app.use(express.static("build"));
 // Catch-all handler -> return index.html
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "build", "index.html"));
-// });
-
-// Function to close server on 'SIGTERM' and 'SIGINT' signals
-const shutdown = signal => async () => {
-  console.log(`${signal} signal received. Closing http server.`);
-
-  try {
-    await client.end();
-    console.log("PostgreSQL client disconnected.");
-  } catch (err) {
-    console.error("Error during disconnection:", err.stack);
-  }
-
-  server.close(err => {
-    if (err) {
-      console.error(err);
-      process.exitCode = 1;
-    }
-    process.exit();
-  });
-};
-
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 const PORT = config.PORT || 3001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-process.on("SIGTERM", shutdown("SIGTERM"));
-process.on("SIGINT", shutdown("SIGINT"));
--- routes/bin-routes.js
+++ routes/bin-routes.js
@@ -3,7 +3,10 @@ const binControllers = require("../controllers/bin-controllers");
 
 const router = express.Router();
 
+// Route create new request bin
+router.post("/api/bin/new", binControllers.createRequestBin);
 // Save request to a bin
+
 router.get("/api/bin/:id", binControllers.getBinForId);
 
 // Delete a request from a bin
@@ -11,9 +14,6 @@ router.delete("/api/bin/:id/:rid", binControllers.deleteRequest);
 
 // Route to view bin's content
 router.post("/bin/:id", binControllers.createRequest);
-
-// Route create new request bin
-router.post("api/bin/new", binControllers.createRequestBin);
 
 // Route to handle GitHub webhook payloads
 router.post("/bin/:id/github-webhook", binControllers.getGihubtPayload);
