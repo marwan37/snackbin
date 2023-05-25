236eb3322ed3350d1160050579a5018af53c29b6
mricasa <michaelr624@gmail.com>
2023-05-24 19:26:52 -0700

    changed to lowercase only

--- utils/turtle.js
+++ utils/turtle.js
@@ -1,6 +1,6 @@
 module.exports = {
   generateRandomString: function (length = 8) {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
     let randomString = "";
     for (let i = 0; i < length; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
